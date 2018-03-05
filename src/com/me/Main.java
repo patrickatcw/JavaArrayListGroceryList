b24c320e46a593156423ab26b13ff8ca7d3b38f2
patrickatcw <pj8392@yahoo.com>
2018-03-05 15:31:39 -0500

    added steps 9 and 10

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,4 +1,27 @@
+//resources
+/*
+https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+https://docs.oracle.com/javase/8/docs/api/java/util/Collection.html
+https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
+https://docs.oracle.com/javase/8/docs/api/java/util/AbstractList.html
+ */
+//arrayList - a resizable array that handles resizing of arrays automatically
+
 package com.me;
 
+//in conjunction with step 9
+import java.util.Scanner;
+
 public class Main {
+
+    //step 9 add scanner
+    private static Scanner scanner = new Scanner(System.in);
+    //step 10
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
