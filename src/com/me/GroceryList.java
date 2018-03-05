1c7e34b0b92cca8add0f9d8d78b31e13438332ec
patrickatcw <pj8392@yahoo.com>
2018-03-05 11:20:22 -0500

    in step 2 we created a new array list

--- src/com/me/GroceryList.java
+++ src/com/me/GroceryList.java
@@ -1,4 +1,20 @@
 package com.me;
+//defining an array ex.
+//int[] myArray = new int[10];
+//numbered 0 - 9
 
+import java.util.ArrayList;
+
+//step 1 created a new class....Grocery List class
 public class GroceryList {
+
+    //step 2  create private variable for our arraylist
+    //different form private int[] myNumbers; because an array list
+    //can hold objects, in case here we are using String
+    //creating an arrow list below, arraylist is actually a class,
+    //note, (), can have it's own constructor
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+
+
 }
